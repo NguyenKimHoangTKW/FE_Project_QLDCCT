aa9bebdb7c5f151f13f1eac99eccbaaaa409c79b
unknown <hoanggnk.it@gmail.com>
2025-11-03 16:32:32 +0700

    Update Module Interface Create Template

--- src/api/DonVi/CreateTemplateAPI.tsx
+++ src/api/DonVi/CreateTemplateAPI.tsx
@@ -2,8 +2,17 @@ import axios from "axios";
 import { URL_API_DONVI } from "../../URL_Config";
 
 export const CreateTemplateAPI = {
+    GetListData: (data: { id_template: number }) =>
+        axios.post(`${URL_API_DONVI}/syllabustemplate/loads-template-section`, data, {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        }).then((res) => res.data),
     GetListCreateTemplate: () =>
         axios.get(`${URL_API_DONVI}/syllabustemplate/load-selected-template`, {
+            withCredentials: true,
+        }).then((res) => res.data),
+    GetListOptionContentType: () =>
+        axios.get(`${URL_API_DONVI}/syllabustemplate/load-option-template-section`, {
             withCredentials: true,
         }).then((res) => res.data),
 
@@ -11,12 +20,32 @@ export const CreateTemplateAPI = {
         id_template: number,
         section_name: string,
         section_code: string,
-        is_required: string,
+        is_required: number,
         order_index: number,
         id_contentType: number,
         id_dataBinding: number,
     }) =>
         axios.post(`${URL_API_DONVI}/syllabustemplate/create-template-section`, data, {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        }).then((res) => res.data),
+    InfoTemplateSection: (data: {
+        id_template_section: number,
+    }) =>
+        axios.post(`${URL_API_DONVI}/syllabustemplate/info-template-section`, data, {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        }).then((res) => res.data),
+    UpdateTemplateSection: (data: {
+        id_template_section: number,
+        section_name: string,
+        section_code: string,
+        is_required: number,
+        order_index: number,
+        id_contentType: number,
+        id_dataBinding: number,
+    }) =>
+        axios.post(`${URL_API_DONVI}/syllabustemplate/update-template-section`, data, {
             headers: { "Content-Type": "application/json" },
             withCredentials: true,
         }).then((res) => res.data),
--- src/modules/DonVi/pages/CreateTemplate.tsx
+++ src/modules/DonVi/pages/CreateTemplate.tsx
@@ -2,14 +2,21 @@ import { useEffect, useState } from "react";
 import { CreateTemplateAPI } from "../../../api/DonVi/CreateTemplateAPI";
 import Select from "react-select";
 import { SweetAlert } from "../../../components/ui/SweetAlert";
+import Modal from "../../../components/ui/Modal";
 
 export default function CreateTemplateInterfaceDonVi() {
     const [listTemplate, setListTemplate] = useState<any[]>([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalMode, setModalMode] = useState<"create" | "edit">("create");
+    const [checkTemplate, setCheckTemplate] = useState(0);
+    const [listContentType, setListContentType] = useState<any[]>([]);
+    const [listDataBinding, setListDataBinding] = useState<any[]>([]);
+    const [listData, setListData] = useState<any[]>([]);
     interface FormData {
         id_template: number | null;
         section_name: string;
         section_code: string;
-        is_required: string;
+        is_required: number | null;
         order_index: number | null;
         id_contentType: number | null;
         id_dataBinding: number | null;
@@ -18,7 +25,7 @@ export default function CreateTemplateInterfaceDonVi() {
         id_template: null,
         section_name: "",
         section_code: "",
-        is_required: "",
+        is_required: null,
         order_index: null,
         id_contentType: null,
         id_dataBinding: null,
@@ -34,18 +41,93 @@ export default function CreateTemplateInterfaceDonVi() {
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
         setFormData((prev) => ({ ...prev, [name]: value }));
+        if (name === "id_template") {
+            console.log(value);
+        }
+        if (name === "id_contentType") {
+            setFormData((prev) => ({ ...prev, id_dataBinding: Number(value) }));
+        }
+        if (name === "id_dataBinding") {
+            setFormData((prev) => ({ ...prev, id_contentType: Number(value) }));
+        }
     }
-    const CreateTemplateSection = async () => {
-        const res = await CreateTemplateAPI.CreateTemplate({
-            id_template: Number(formData.id_template),
-            section_name: formData.section_name,
-            section_code: formData.section_code,
-            is_required: formData.is_required,
-            order_index: Number(formData.order_index),
-            id_contentType: Number(formData.id_contentType),
-            id_dataBinding: Number(formData.id_dataBinding),
-        });
+    const LoadOptionSection = async () => {
+        const res = await CreateTemplateAPI.GetListOptionContentType();
+        setListContentType(res.contentType);
+        setListDataBinding(res.dataBinding);
+        setFormData((prev) => ({ ...prev, id_contentType: Number(res.contentType[0].id), id_dataBinding: Number(res.dataBinding[0].id), is_required: 1 }));
+    }
+    const InfoTemplateSection = async (id: number) => {
+        const res = await CreateTemplateAPI.InfoTemplateSection({ id_template_section: id });
         if (res.success) {
+            setFormData({
+                id_template: res.data.id_template,
+                section_name: res.data.section_name,
+                section_code: res.data.section_code,
+                is_required: res.data.is_required,
+                order_index: res.data.order_index,
+                id_contentType: res.data.id_contentType,
+                id_dataBinding: res.data.id_dataBinding,
+            });
+            setModalMode("edit");
+            setModalOpen(true);
+        }
+        else {
+            SweetAlert("error", res.message);
+        }
+    }
+    const handleAddNewTemplateSection = () => {
+        if (checkTemplate > 0) {
+            setModalMode("create");
+            setModalOpen(true);
+        }
+        else {
+            SweetAlert("error", "Vui lòng chọn mẫu đề cương");
+            return;
+        }
+    }
+    const handleUpdateTemplateSection = async () => {
+        if(modalMode === "create") {
+            const res = await CreateTemplateAPI.CreateTemplate({
+                id_template: checkTemplate,
+                section_name: formData.section_name,
+                section_code: formData.section_code,
+                is_required: Number(formData.is_required),
+                order_index: Number(formData.order_index),
+                id_contentType: Number(formData.id_contentType),
+                id_dataBinding: Number(formData.id_dataBinding),
+            });
+            if (res.success) {
+                SweetAlert("success", res.message);
+                loadDataTemplate();
+            }
+            else {
+                SweetAlert("error", res.message);
+            }
+        }
+        else {
+            const res = await CreateTemplateAPI.UpdateTemplateSection({
+                id_template_section: Number(formData.id_template),
+                section_name: formData.section_name,
+                section_code: formData.section_code,
+                is_required: Number(formData.is_required),
+                order_index: Number(formData.order_index),
+                id_contentType: Number(formData.id_contentType),
+                id_dataBinding: Number(formData.id_dataBinding),
+            });
+            if (res.success) {
+                SweetAlert("success", res.message);
+                loadDataTemplate();
+            }
+            else {
+                SweetAlert("error", res.message);
+            }
+        }
+    }
+    const loadDataTemplate = async () => {
+        const res = await CreateTemplateAPI.GetListData({ id_template: checkTemplate });
+        if (res.success) {
+            setListData(res.data);
             SweetAlert("success", res.message);
         }
         else {
@@ -53,6 +135,7 @@ export default function CreateTemplateInterfaceDonVi() {
         }
     }
     useEffect(() => {
+        LoadOptionSection();
         LoadSelectTemplate();
     }, []);
     return (
@@ -61,7 +144,7 @@ export default function CreateTemplateInterfaceDonVi() {
                 <div className="card-body">
                     <div className="page-header no-gutters">
                         <h2 className="text-uppercase">
-                            Quản lý Danh sách Chuẩn đầu ra học phần
+                            Quản lý Danh sách dạng câu hỏi cho đề cương học phần
                         </h2>
                         <hr />
                         <fieldset className="border rounded-3 p-3">
@@ -74,46 +157,146 @@ export default function CreateTemplateInterfaceDonVi() {
                                         isClearable
                                         onChange={(option: any) => setFormData((prev) => ({
                                             ...prev,
-                                            id_template: Number(option ? option.value : null),
+                                            id_template: Number(option.value),
                                         }))}
+                                        onBlur={() => setCheckTemplate(Number(formData.id_template))}
                                     />
                                 </div>
                             </div>
 
                             <div className="row">
                                 <div className="col-12 d-flex flex-wrap gap-2 justify-content-start justify-content-md-end">
-                                    <button className="btn btn-primary">
+                                    <button className="btn btn-primary" onClick={loadDataTemplate}>
                                         <i className="fas fa-plus-circle mr-1" /> Lọc dữ liệu
                                     </button>
                                 </div>
                             </div>
                         </fieldset>
                     </div>
+                    <div className="card mt-3">
+                        <div className="card-body">
+                            {listData.map((item) => (
+                                <div
+                                    key={item.id_template_section}
+                                    className="card mb-4 border-0 shadow-sm"
+                                >
+                                    <div className="card-body bg-light rounded-3">
+                                        <p className="text-muted mb-3">
+                                            <i className="fas fa-list-ol me-3"></i>
+                                            <span className="ms-1"> Thứ tự hiển thị: </span>
+                                            <strong className="ms-2">{item.order_index}</strong>
+                                        </p>
+
+                                        <hr />
+                                        <div className="d-flex justify-content-between align-items-start mb-2">
+                                            <h5 className="fw-bold text-dark mb-0">
+                                                {item.section_code}. {item.section_name}
+                                            </h5>
+                                            <div className="d-flex gap-2">
+                                                <button className="btn btn-outline-primary btn-sm" onClick={() => InfoTemplateSection(item.id_template_section)}>
+                                                    <i className="fas fa-edit me-1"></i> Sửa
+                                                </button>
+                                                <button className="btn btn-outline-danger btn-sm">
+                                                    <i className="fas fa-trash me-1"></i> Xóa
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <hr />
+
+                                        <div className="ps-2">
+                                            <p className="text-primary mb-2">
+                                                <strong className="text-dark">Dạng câu hỏi:</strong> {item.contentType}
+                                            </p>
+                                            <p className="text-primary mb-2">
+                                                <strong className="text-dark">Bắt buộc:</strong> {item.is_required}
+                                            </p>
+                                            <p className="text-primary mb-0">
+                                                <strong className="text-dark">Liên kết dữ liệu:</strong> {item.dataBinding}
+                                            </p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="text-center border-top pt-3 d-flex justify-content-center gap-3 flex-wrap">
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                onClick={handleAddNewTemplateSection}
+                            >
+                                <i className="fas fa-plus-circle me-1"></i> Tạo mới tiêu đề
+                            </button>
+
+                            <button
+                                type="button"
+                                id="btnPreviewTemplateCreated"
+                                className="btn btn-outline-primary"
+                            >
+                                <i className="fas fa-eye me-1"></i> Xem trước mẫu đề cương
+                            </button>
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
-            <div id="smallBanner" className="small-banner fixed-footer">
-                <button type="button" id="btnAddTitle" className="btn btn-primary m-r-5">
-                    Tạo mới tiêu đề cha
-                </button>
-                <button type="button" id="btnAddChilTitle" className="btn btn-warning m-r-5">
-                    Tạo mới tiêu đề con
-                </button>
-                <button type="button" id="btnSortTitle" className="btn btn-secondary m-r-5">
-                    Sắp xếp lại thứ tự câu hỏi
-                </button>
-                <button
-                    type="button"
-                    id="btnXemTruocPhieuDaTao"
-                    className="btn btn-info m-r-5"
-                >
-                    Xem trước Form phiếu khảo sát
-                </button>
-                <button type="button" id="btnXuatBanPhieu" className="btn btn-success m-r-5">
-                    Xuất bản phiếu
-                </button>
-            </div>
-
+            <Modal
+                isOpen={modalOpen}
+                title={modalMode === "create" ? "Tạo mới tiêu đề" : "Chỉnh sửa tiêu đề"}
+                onClose={() => setModalOpen(false)}
+                onSave={handleUpdateTemplateSection}
+            >
+                <form id="modal-body" autoComplete="off">
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label">Thứ tự</label>
+                        <div className="col-sm-10">
+                            <input type="number" className="form-control" name="order_index" value={formData.order_index ?? ""} onChange={handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label">Tên tiêu đề</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" name="section_name" value={formData.section_name ?? ""} onChange={handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label">Mã tiêu đề</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" name="section_code" value={formData.section_code ?? ""} onChange={handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label">Yêu cầu</label>
+                        <div className="col-sm-10">
+                            <select className="form-control" name="is_required" value={formData.is_required ?? ""} onChange={handleInputChange} >
+                                <option value="1">Bắt buộc</option>
+                                <option value="0">Không bắt buộc</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label">Loại nội dung</label>
+                        <div className="col-sm-10">
+                            <select className="form-control" name="id_contentType" value={formData.id_contentType ?? ""} onChange={handleInputChange} >
+                                {listContentType.map((item: any) => (
+                                    <option key={item.id} value={item.id}>{item.name}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label">Liên kết dữ liệu</label>
+                        <div className="col-sm-10">
+                            <select className="form-control" name="id_dataBinding" value={formData.id_dataBinding ?? ""} onChange={handleInputChange} >
+                                {listDataBinding.map((item: any) => (
+                                    <option key={item.id} value={item.id}>{item.name}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </Modal>
         </div>
     )
 }
